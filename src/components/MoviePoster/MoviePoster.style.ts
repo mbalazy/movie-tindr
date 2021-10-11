37582a91f51b505309d878fe2a054479e1671b80
mbalazy <p@mbalazy.pl>
2021-10-11 11:30:04 +0200

    Fix border-radius on MoviePoster

--- src/components/MoviePoster/MoviePoster.style.ts
+++ src/components/MoviePoster/MoviePoster.style.ts
@@ -4,6 +4,7 @@ export const MoviePosterWrapper = styled.div`
   grid-column: 1 / span 1;
   grid-row: 1/-1;
   box-shadow: 0px 7px 56px 1px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
 
   ${({ theme: { down, breakpoints } }) => css`
     ${down(breakpoints.l)} {
