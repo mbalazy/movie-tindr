8cadb3f40776c840f7f491fac561b7d61945f89b
mbalazy <p@mbalazy.pl>
2021-10-12 14:35:48 +0200

    Update DATA

--- src/DATA.ts
+++ src/DATA.ts
@@ -1,42 +1,82 @@
 export const DUMMY_DATA = [
   {
-    id: "slakshf",
+    id: "1",
     imageUrl: "https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
     title: "Venom",
     summary:
       "Investigative journalist Eddie Brock attempts a comeback following a scandal, but accidentally becomes the host of Venom, a violent, super powerful alien symbiote. Soon, he must rely on his newfound powers to protect the world from a shadowy organization looking for a symbiote of their own.",
+    rating: "8.3",
+  },
+  {
+    id: "2",
+    imageUrl: "https://image.tmdb.org/t/p/w500/dDlEmu3EZ0Pgg93K2SVNLCjCSvE.jpg",
+    title: "Squid Game",
+    summary:
+      "Hundreds of cash-strapped players accept a strange invitation to compete in children's games—with high stakes. But, a tempting prize awaits the victor.",
+    rating: "7.9",
+  },
+  {
+    id: "3",
+    imageUrl: "https://image.tmdb.org/t/p/w500/lztz5XBMG1x6Y5ubz7CxfPFsAcW.jpg",
+    title: "What If...?",
+    summary:
+      "Taking inspiration from the comic books of the same name, each episode explores a pivotal moment from the Marvel Cinematic Universe and turns it on its head, leading the audience into uncharted territory.",
+    rating: "8.3",
+  },
+  {
+    id: "4",
+    imageUrl: "https://image.tmdb.org/t/p/w500/ktDJ21QQscbMNQfPpZBsNORxdDx.jpg",
+    title: "Lucifer",
+    summary:
+      "Bored and unhappy as the Lord of Hell, Lucifer Morningstar abandoned his throne and retired to Los Angeles, where he has teamed up with LAPD detective Chloe Decker to take down criminals. But the longer he's away from the underworld, the greater the threat that the worst of humanity could escape.",
+    rating: "8.5",
+  },
+  {
+    id: "5",
+    imageUrl: "https://image.tmdb.org/t/p/w500/9NQybnf78WiXWcxPaf7cyF09bOm.jpg",
+    title: "El Juego de las Llaves",
+    summary:
+      "A casual encounter between Adriana and Sergio, two former high school friends, triggers the beginning of the game of the keys where four couples generate new sexual and emotional combinations in a dangerous and lustful game.",
     rating: "5.3",
   },
   {
-    id: "jfhgjfh",
-    imageUrl: "https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
-    title: "alsjfh",
+    id: "6",
+    imageUrl: "https://image.tmdb.org/t/p/w500/w21lgYIi9GeUH5dO8l3B9ARZbCB.jpg",
+    title: "The Walking Dead",
     summary:
-      "Investigative journalist Eddie Brock attempts a comeback following a scandal, but accidentally becomes the host of Venom, a violent, super powerful alien symbiote. Soon, he must rely on his newfound powers to protect the world from a shadowy organization looking for a symbiote of their own.",
-    rating: "5.3",
+      "Sheriff's deputy Rick Grimes awakens from a coma to find a post-apocalyptic world dominated by flesh-eating zombies. He sets out to find his family and encounters many other survivors along the way.",
+    rating: "8.1",
   },
   {
-    id: "kghfkg",
-    imageUrl: "https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
-    title: "kasjfgg",
+    id: "7",
+    imageUrl: "https://image.tmdb.org/t/p/w500/4brWcSXdH31BZUTtRTHj2BYFe6M.jpg",
+    title: "Maid",
     summary:
-      "Investigative journalist Eddie Brock attempts a comeback following a scandal, but accidentally becomes the host of Venom, a violent, super powerful alien symbiote. Soon, he must rely on his newfound powers to protect the world from a shadowy organization looking for a symbiote of their own.",
-    rating: "5.3",
+      "After fleeing an abusive relationship, a young mother finds a job cleaning houses and fights to provide a better future for her child.",
+    rating: "7.9",
   },
   {
-    id: "llr32",
-    imageUrl: "https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
-    title: "33tjcv",
+    id: "8",
+    imageUrl: "https://image.tmdb.org/t/p/w500/dsAJhCLYX1fiNRoiiJqR6Up4aJ.jpg",
+    title: "Rachael Ray",
     summary:
-      "Investigative journalist Eddie Brock attempts a comeback following a scandal, but accidentally becomes the host of Venom, a violent, super powerful alien symbiote. Soon, he must rely on his newfound powers to protect the world from a shadowy organization looking for a symbiote of their own.",
-    rating: "5.3",
+      "Rachael Ray, also known as The Rachael Ray Show, is an American talk show starring Rachael Ray that debuted in syndication in the United States and Canada on September 18, 2006. It is filmed at Chelsea Television Studios in New York City.",
+    rating: "6.4",
   },
   {
-    id: "alkdgh",
-    imageUrl: "https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
-    title: "lflvlvksad",
+    id: "9",
+    imageUrl: "https://image.tmdb.org/t/p/w500/cXUqtadGsIcZDWUTrfnbDjAy8eN.jpg",
+    title: "The Good Doctor",
     summary:
-      "Investigative journalist Eddie Brock attempts a comeback following a scandal, but accidentally becomes the host of Venom, a violent, super powerful alien symbiote. Soon, he must rely on his newfound powers to protect the world from a shadowy organization looking for a symbiote of their own.",
-    rating: "5.3",
+      "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives",
+    rating: "8.9",
+  },
+  {
+    id: "10",
+    imageUrl: "https://image.tmdb.org/t/p/w500/5qQWZOSD1xn2lNge7kd4QaPTN3O.jpg",
+    title: "Les Mutants",
+    summary:
+      "In Notre-Dame-du-Lointain, a village far from major urban centers, three young people make a strange discovery. In an abandoned barn, a family of mutants is hiding: human-like beings, but struggling with horrible animal growths.",
+    rating: "6.3",
   },
 ];
