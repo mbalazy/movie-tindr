c3dc60f2ed96f905c4654ab2defb7ea70a1cf37f
mbalazy <p@mbalazy.pl>
2021-10-12 15:40:09 +0200

    Fix Footer on mobile

--- src/components/Main/Main.style.ts
+++ src/components/Main/Main.style.ts
@@ -2,8 +2,6 @@ import styled, { css } from "styled-components";
 
 export const MainWrapper = styled.main`
   margin: 3rem;
-  min-height: calc(90vh);
-  overflow: hidden;
 
   ${({ theme: { down, breakpoints } }) => css`
     ${down(breakpoints.l)} {
@@ -14,11 +12,6 @@ export const MainWrapper = styled.main`
 
 export const MainInnerWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
-  position: absolute;
-  margin-left: auto;
-  margin-right: auto;
-  left: 20px;
-  right: 20px;
   padding: 3rem;
   border-radius: 6px;
   min-height: calc(100vh - 14rem);
